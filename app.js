3db0581ae39685e98ee9dedef242e45f48187b58
laozhao <3@3.com>
2017-01-10 01:57:34 +0800

    提交

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/', main);
 app.use('/index', users);
  
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handlergit
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
